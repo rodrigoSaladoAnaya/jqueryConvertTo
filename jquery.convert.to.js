a996957d68d633ad31b17cb6d0d4d63ecc7b9ea3
rodrigoSaladoAnaya <rodrigosaladoanaya@yahoo.com.mx>
2015-02-05 02:47:54 -0600

    Try to fix the scope error, : (

--- jquery.convert.to.js
+++ jquery.convert.to.js
@@ -13,55 +13,55 @@
                 defaultFncs
             );
         }
-                
-        if(htmlType == 'input' || htmlType == 'select') {
+
+        if($html.is(':input') || $html.is(':select')) {
             $.each($.fn.convertTo.defaults, function(k, f){
                 if(f === undefined) {
                     $html[k]();
                 } else if(typeof f === 'function') {
-                    $.fn.convertTo.$html = $html;
-                    $.fn.convertTo.$elem = $elem;
-                    $.fn.convertTo.fnc = fnc;                   
-                    
-                    $html.on(k, f)
+                    var nf = f.bind({
+                        $html: $html,
+                        $elem: $elem,
+                        fnc: fnc
+                    });
+                    $html.on(k, nf)
                 } else {
-                    try {
-                        $html[k](eval(f));
-                    } catch(e) {
-                        $html[k](f);
+                    var isKeyword = k === '$html' 
+                                    || k === '$elem' 
+                                    || k === 'fnc';
+                    if(!isKeyword){                        
+                        try {
+                            $html[k](eval(f));
+                        } catch(e) {
+                            $html[k](f);
+                        }    
                     }
                 }
             });
         }        
-    }  
-
-    $.fn.convertTo.$html = null;
-    $.fn.convertTo.$elem = null;
-    $.fn.convertTo.fnc = undefined;
-
+    }
+    
     $.fn.convertTo.defaults = {
+        $html: null, 
+        $elem: null,
+        fnc: null,
         val: 'elemVal',
         focus: undefined,
         keyup: function(event) {
-            $html = $.fn.convertTo.$html;
-            $elem = $.fn.convertTo.$elem;
-            fnc = $.fn.convertTo.fnc;
             if (event.keyCode == 13) { //Intro
-                var newVal = $html.val();
-                $elem.html(newVal);
-                if(fnc !== undefined) {
-                    fnc.call($elem, newVal);
+                var newVal = this.$html.val();
+                this.$elem.html(newVal);
+                if($fnc !== undefined) {
+                    $fnc.call(this.$elem, newVal);
                 }
-                $html.replaceWith($elem);
+                this.$html.replaceWith(this.$elem);
                 event.preventDefault();
             } else if (event.keyCode == 27) { //Esc
-               $html.replaceWith($elem);
+               $html.replaceWith(this.$elem);
             }            
         },
         focusout: function() {
-            $html = $.fn.convertTo.$html;
-            $elem = $.fn.convertTo.$elem;
-            $html.replaceWith($elem);
+            this.$html.replaceWith(this.$elem);
         }
     }
 })(jQuery);
