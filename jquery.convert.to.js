b8d8588706c94967848fd18e71087379811f479f
rodrigoSaladoAnaya <rodrigosaladoanaya@yahoo.com.mx>
2015-02-05 00:26:57 -0600

    The plugin now can replace the old element with the new one, only for inputs and selects tags. By default apply several functions like val, focus, keyup and focusout, but you will can add other functions, the order is important  you need to be careful.

--- jquery.convert.to.js
+++ jquery.convert.to.js
@@ -1,2 +1,60 @@
-(function($){	
+(function($){
+
+    $.fn.convertTo = function (html, fnc, defaultFncs) {
+        var $html = $(html);
+        var htmlType = $html.prop('tagName')
+                            .toLowerCase();
+        var $elem = $(this).replaceWith($html);
+        var elemVal = $elem.html();        
+                
+        if(htmlType == 'input' || htmlType == 'select') {
+            $.each($.fn.convertTo.defaults, function(k, f){
+                if(f === undefined) {
+                    $html[k]();
+                } else if(typeof f === 'function') {
+                    $.fn.convertTo.$html = $html;
+                    $.fn.convertTo.$elem = $elem;
+                    $.fn.convertTo.fnc = fnc;                   
+                    
+                    $html.on(k, f)
+                } else {
+                    try {
+                        $html[k](eval(f));
+                    } catch(e) {
+                        $html[k](f);
+                    }
+                }
+            });
+        }        
+    }  
+
+    $.fn.convertTo.$html = null;
+    $.fn.convertTo.$elem = null;
+    $.fn.convertTo.fnc = undefined;
+
+    $.fn.convertTo.defaults = {
+        val: 'elemVal',
+        focus: undefined,
+        keyup: function(event) {
+            $html = $.fn.convertTo.$html;
+            $elem = $.fn.convertTo.$elem;
+            fnc = $.fn.convertTo.fnc;
+            if (event.keyCode == 13) { //Intro
+                var newVal = $html.val();
+                $elem.html(newVal);
+                if(fnc !== undefined) {
+                    fnc.call($elem, newVal);
+                }
+                $html.replaceWith($elem);
+                event.preventDefault();
+            } else if (event.keyCode == 27) { //Esc
+               $html.replaceWith($elem);
+            }            
+        },
+        focusout: function() {
+            $html = $.fn.convertTo.$html;
+            $elem = $.fn.convertTo.$elem;
+            $html.replaceWith($elem);
+        }
+    }
 })(jQuery);
